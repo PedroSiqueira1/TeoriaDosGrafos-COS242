696919714591cf06a9c2f25399a6cec7ffb71e71
Pedro Siqueira das Neves <pedro.siqueira@poli.ufrj.br>
2021-12-18 17:34:00 -0300

    Merge branch 'main' of https://github.com/PedroSiqueira1/TeoriaDosGrafos-COS242

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <sstream>
 #include <string>
 #include <list>
+#include <vector>
+#include <algorithm>
+
+
 using namespace std;
 
 
@@ -60,13 +64,16 @@ private:
     int N;
     int min_degree;
     int max_degree = 0;
+    vector<int> degrees; 
     int M = 0;
+    double sum = 0;
     string str;
     list<int> *L;
 
 public:
     
     AdjListGraph(string str){
+        
         this-> str = str;
         N = (int)str.at(0) - '0';
         int len = str.length();
@@ -94,46 +101,72 @@ public:
             cout<<endl;
         }
     }
-
+    
     void numOfEdges(){
         cout << "M is " << M << '\n';
     }
 
+    double findMean(){
+        return sum/N;
+    }
+
+
+
+    int findMedian(){
+        std::sort(std::begin(degrees), std::end(degrees));
+        if (N % 2 != 0){
+            return degrees.at(N / 2);
+        }
+        else{
+            return degrees.at((((N - 1) / 2) + degrees.at(N / 2)) / 2);
+        }
+    }
+
     void find_degrees(){
-        list<int> degrees;
-        double sum = 0;
         for(int i = 0; i < N; i++){
-            degrees.push_back(L[i].size());
-            sum += L[i].size();
+
+            degrees.push_back((L[i].size())); // Adicionando grau do vértice à lista
+            sum += L[i].size(); // Somando os graus dos vértices
 
             if(!min_degree){
                 min_degree = L[i].size();
             }
-
             if (L[i].size() < min_degree){
                 min_degree = L[i].size();
             }
+
             if (L[i].size() > max_degree){
                 max_degree = L[i].size();
             }
         }
 
+        ofstream answer;
+        answer.open("Resposta.txt");
+        if (answer.is_open()){
+            answer << "O grafo possui " << to_string(N) << " vértices \n";
+            answer << "O grau mínimo é " << min_degree<< '\n';
+            answer << "O grau máximo é " << max_degree<< '\n';
+            answer << "A média dos graus é " << findMean() << '\n';
+            answer << "A mediana dos graus é " << findMedian() << '\n';
+            answer.close();
 
-        cout << min_degree<< '\n';
-        cout << max_degree<< '\n';
-        cout << sum/N << '\n';
+        }
+        
+
+        cout << "O grau mínimo é " << min_degree<< '\n';
+        cout << "O grau máximo é " << max_degree<< '\n';
+        cout << "A média dos graus é " << findMean() << '\n'; // Média calculada dividindo a soma dos vértices pelo número de vértices
+        cout << "A mediana dos graus é " << findMedian() << '\n'; 
     }
 
 };
 
 
 
-int main()
-{
+int main(){
 
     string s = "6\n12\n13\n14\n15\n23\n24\n25\n34\n35\n45\n01";
     int vertices;
-
     AdjListGraph g(s);
     g.printList();
     //g.numOfEdges();
